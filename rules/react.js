ade1b7ce0df63332b8705cdbf49f2d5d626915ac
anlyyao <anly_yaw@163.com>
2022-04-28 10:24:56 +0800

    feat: Eslint config init, based on airbnb-base

---
+++ rules/react.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'react-hooks/exhaustive-deps': 'warn',
+  'react-hooks/rules-of-hooks': 'error',
+  'react/display-name': 'off',
+  'react/prop-types': 'off',
+
+  'lodash/import-scope': [2, 'method'],
+};
---
+++ vue-next.js
@@ -0,0 +1,60 @@
+const vueRule = require('./rules/vue');
+
+module.exports = {
+  extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
+  plugins: ['vue'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    extraFileExtensions: ['.vue'],
+  },
+  globals: {
+    cy: 'readonly',
+    clipboardData: 'readonly',
+    PKG_VERSION: true,
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  rules: {
+    ...vueRule,
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'vue/component-name-in-template-casing': [2, 'kebab-case'],
+        'vue/require-default-prop': 0,
+        'vue/multi-word-component-names': 0,
+        'vue/no-reserved-props': 0,
+      },
+    },
+    {
+      files: ['**/demos/*', 'script/**/*', 'script/*', '*.js', 'site/**/*', 'site/*'],
+      rules: {
+        'no-var-requires': 0,
+        'no-console': 0,
+        'no-unused-expressions': 0,
+        'no-alert': 0,
+      },
+    },
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'prefer-spread': 0,
+        '@typescript-eslint/explicit-function-return-type': 0,
+      },
+    },
+    {
+      files: ['*.test.js'],
+      rules: {
+        'import/no-dynamic-require': 'off',
+        'global-require': 'off',
+      },
+    },
+    {
+      files: '*',
+    },
+  ],
+};
